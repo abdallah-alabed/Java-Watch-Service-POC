c8a33b48f85f0f323700c345d65903648a467fa9
A.AlAbed01-L <A.AlAbed01-L@JBS-LP-0074.midisglobal.local>
2024-01-04 13:35:45 +0300

    Initial Commit

---
+++ src/main/java/com/JBS/IIAB_POC_Folder_Listen/IiabPocFolderListenApplication.java
@@ -0,0 +1,13 @@
+package com.JBS.IIAB_POC_Folder_Listen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiabPocFolderListenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiabPocFolderListenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JBS/IIAB_POC_Folder_Listen/IiabPocFolderListenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JBS.IIAB_POC_Folder_Listen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiabPocFolderListenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
